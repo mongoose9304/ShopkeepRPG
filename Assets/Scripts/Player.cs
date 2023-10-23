741ed2e51a05752f464bb67c34addd3128b436bf
dgmode21 <56322304+dgmode21@users.noreply.github.com>
2023-10-23 00:40:59 -0400

    Movement  Updates
    Movement feels good i like it.
    
     But the jump still needs condition code to test if player is on ground.
    
    Small adjustments in future. but i feel a good start.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     Rigidbody rb;
 
     public float speed = 3;
-
+    Vector3 Vec;
 
 
     // Start is called before the first frame update
@@ -26,9 +26,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TestingUpdate();
+       
+        // just a simple jump function
+        //works well with the movement of player.
+        //speed can be adjusted by increasing the Y and or increase the 6 to more.
+        // the speed to side to side maybe can be less but i like the movement and jump 
+        //movement feels ok i think.
 
-        // Testing out the movement of player.
+
+        //also the jump though needs a condition if player is touching ground but thats after.
+        //im happy with this for starting out movement but can be adjusted as we go.
 
         if (Input.GetKeyDown("space"))
         {
@@ -36,25 +43,18 @@ public class Player : MonoBehaviour
 
         }
 
-
-
-
+        Vec = transform.localPosition;
+        
+        Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 20;
+        Vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 20;
+        transform.localPosition = Vec;
 
        
+
+
+
+
     }
 
-
-    //throwing in random functions can be moved after to whatever or stay.
    
-    
-    private void TestingUpdate()
-    {
-        Vector3 MovingAllDirections = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-
-
-    }
-
 }
