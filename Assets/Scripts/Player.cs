d893bb300294165d3372a54e7146402808549884
dgmode21 <56322304+dgmode21@users.noreply.github.com>
2023-10-22 23:55:11 -0400

    the movement feels great.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  //Random Varible ideas
+
+    Rigidbody rb;
+
+    public float speed = 3;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        TestingUpdate();
+
+        // Testing out the movement of player.
+
         if (Input.GetKeyDown("space"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 6, 0); 
+            rb.velocity = new Vector3(0, 6, 0);
+
         }
+
+
+
+
+
+       
     }
+
+
+    //throwing in random functions can be moved after to whatever or stay.
+   
+    
+    private void TestingUpdate()
+    {
+        Vector3 MovingAllDirections = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+
+    }
+
 }
