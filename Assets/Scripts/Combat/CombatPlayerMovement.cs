3d59d3e1ff87b87864027feba8793b8da206c6b3
RobertWard <robert.blank.wardo@gmail.com>
2024-02-22 18:03:15 -0500

    Starting Combat movement

---
+++ Assets/Scripts/Combat/CombatPlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 2.5f;
+
+    [SerializeField] private float turnSpeed = 720;
+    [SerializeField] private Transform model;
+    private Vector3 input;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetPlayerInputs();
+        PlayerLook();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+       
+    }
+
+    void GetPlayerInputs()
+    {
+        // gets both controller and keyboard input. theres a better way for gamepad using 
+        // Vector2 stickValue = Gamepad.current.leftStick.ReadValue();
+        // input = new Vector3(stickValue.x, 0, stickValue.y);
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    void Move()
+    {
+        // for now, this is fine enough for movement, the 'SkewToIso' functioon is inside of the IsoGlobals, 
+        // just multiplies it by matrix to align movements to 45 degree angle.
+        // If we want jumping we may need to find another way to handle player movement, if you jump and dont press any buttons
+        // you'll just fall vertical.
+        if (input != Vector3.zero)
+        {
+            Vector3 movement = input * input.normalized.magnitude * speed * Time.deltaTime;
+            rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+    void PlayerLook()
+    {
+        // TODO: add a model for testin
+        if (input == Vector3.zero) return;
+
+        // this logic is specifically for having a model be a child of the gameobject with a rigidbody (rigidbody doesn't rotate)
+        Quaternion rotation = Quaternion.LookRotation(input, Vector3.up);
+        model.rotation = Quaternion.RotateTowards(model.rotation, rotation, turnSpeed * Time.deltaTime);
+
+    }
+}
