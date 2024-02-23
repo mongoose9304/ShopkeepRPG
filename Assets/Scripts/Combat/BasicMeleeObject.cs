08cdf33a90cd75680f0b9bd5af505fcef7044e3e
RobertWard <robert.blank.wardo@gmail.com>
2024-02-22 19:12:25 -0500

    combat progress, kinda looks like a man sweeping

---
+++ Assets/Scripts/Combat/BasicMeleeObject.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMeleeObject : MonoBehaviour
+{
+    [SerializeField] private float swingSpeed;
+    [SerializeField] private Quaternion startRotaton;
+    [SerializeField] private Quaternion startRotatonB;
+    [SerializeField] private float attackDurationMax;
+     private float attackDurationCurrent;
+    [SerializeField] private float damage;
+    [SerializeField] private Element damageType;
+    [SerializeField] private float hitStun;
+    [SerializeField] private GameObject weaponObject;
+    //make private after test
+    [SerializeField] private bool queuedAttack;
+    [SerializeField] private bool rightAttackDirection;
+
+    private void Update()
+    {
+        if(weaponObject.activeInHierarchy)
+        {
+            if(rightAttackDirection)
+            transform.Rotate(0.0f, swingSpeed*Time.deltaTime, 0.0f, Space.Self);
+            else
+                transform.Rotate(0.0f, -swingSpeed * Time.deltaTime, 0.0f, Space.Self);
+
+            attackDurationCurrent -= Time.deltaTime;
+            if (attackDurationCurrent<=0)
+                EndAttack();
+
+            //queuedAttack = false;
+        }
+    }
+
+
+    public void StartAttack()
+    {
+        transform.localRotation = startRotaton;
+        weaponObject.SetActive(true);
+        attackDurationCurrent = attackDurationMax;
+    }
+    public void EndAttack()
+    {
+        if(queuedAttack)
+        {
+            rightAttackDirection = !rightAttackDirection;
+            attackDurationCurrent = attackDurationMax;
+            queuedAttack=false;
+        }
+        else
+        weaponObject.SetActive(false);
+    }
+    public bool TryToAttack()
+    {
+        if(weaponObject.gameObject.activeInHierarchy)
+        {
+            queuedAttack = true;
+            return false;
+        }
+        else
+        {
+            StartAttack();
+            rightAttackDirection = true;
+            return true;
+        }
+    }
+   
+}
---
+++ Assets/Scripts/Combat/CombatPlayerActions.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatPlayerActions : MonoBehaviour
+{
+    
+    [Header("BasicMelee")]
+    [SerializeField] private float BasicMeleeCooldownMax;
+    private float BasicMeleeCooldown = 0.0f;
+    [SerializeField] private GameObject BasicMeleePivotObject;
+    [SerializeField] BasicMeleeObject meleeObject;
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1")&&BasicMeleeCooldown<=0)
+        {
+           
+            BasicMelee();
+          
+        }
+        Cooldowns();
+    }
+
+    private void Cooldowns()
+    {
+        BasicMeleeCooldown -= Time.deltaTime;
+    }
+
+    private void BasicMelee()
+    {
+        if(meleeObject.TryToAttack())
+        {
+            BasicMeleeCooldown =BasicMeleeCooldownMax;
+        }
+    }
+}
