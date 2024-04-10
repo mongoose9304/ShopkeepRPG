7d17fe0585fa46cb16469d37ab46ea6cc25dc5d1
RobertWard <robert.blank.wardo@gmail.com>
2024-04-09 20:47:53 -0400

    magnet adjustments

--- Assets/Scripts/Combat/Loot/MagnetPuller.cs
+++ Assets/Scripts/Combat/Loot/MagnetPuller.cs
@@ -12,7 +12,7 @@ public class MagnetPuller : MonoBehaviour
     private Rigidbody rb;
     private void OnTriggerEnter(Collider other)
     {
-        return;
+       
         foreach(string tag_ in pullableTags)
         {
             if(other.tag==tag_)
@@ -25,7 +25,7 @@ public class MagnetPuller : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        return;
+       
         foreach (string tag_ in pullableTags)
         {
             if (other.tag == tag_)
@@ -38,27 +38,17 @@ public class MagnetPuller : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider other)
+  
+    private void FixedUpdate()
     {
-        foreach (string tag_ in pullableTags)
-        {
-            if (other.tag == tag_)
-            {
-               rb= other.GetComponent<Rigidbody>();
-                rb.AddForce((transform.position-other.transform.position).normalized*pullSpeed*Time.deltaTime,ForceMode.Impulse);
-            }
-        }
-    }
-    private void Update()
-    {
-        return;
+        
         objectsToPull.RemoveAll(x => !x);
         foreach (GameObject obj in objectsToPull)
         {
-          
 
-            Vector3 temp = Vector3.Lerp(obj.transform.position, this.transform.position, pullSpeed * Time.deltaTime);
-            obj.transform.position = Vector3.SmoothDamp(obj.transform.position, temp, ref velocity, dampModifier);
+
+            rb = obj.GetComponent<Rigidbody>();
+            rb.AddForce((transform.position - obj.transform.position).normalized * pullSpeed * Time.deltaTime, ForceMode.VelocityChange);
         }
        // objectsToPull.Clear();
 
