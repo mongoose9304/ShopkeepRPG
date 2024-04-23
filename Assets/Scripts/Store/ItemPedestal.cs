bd61d1deb4da6e4d27895fb760a0856f75a82dd3
mongoose9304 <rymaster_109@hotmail.com>
2024-04-23 14:20:34 -0400

    Store Progress

---
+++ Assets/Scripts/Store/ItemPedestal.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPedistal : MonoBehaviour
+{
+    // Holds a gameobject
+    public GameObject item;
+}
---
+++ Assets/Scripts/Store/StoreManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This class is responsible for managing the store UI and functionality. It will hold an array of gameobjects for each item pedistal
+public class StoreManager : MonoBehaviour
+{
+    // StoreManager holds all the item pedestals, they can be empty or holding an item
+    public GameObject[] itemPedestals;
+
+    // We need to know the specific checkout pedestal
+    public GameObject checkoutPedestal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
