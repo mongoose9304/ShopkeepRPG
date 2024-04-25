e2415e6ac2d33da7a22b358ae9cba6bd8b126d6d
RobertWard <robert.blank.wardo@gmail.com>
2024-04-25 09:49:04 -0400

    working on basic combat room

--- Assets/Scripts/Combat/Dungeons/Rooms/CombatRoom.cs
+++ Assets/Scripts/Combat/Dungeons/Rooms/CombatRoom.cs
@@ -6,14 +6,55 @@ public class CombatRoom : BasicRoom
 {
     bool isLocked;
 
-
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+    [SerializeField] private int maxEnemies;
+    [SerializeField] private int instantEnemies;
+    [SerializeField] private int enemiesLeft;
+    [SerializeField] private float spawnDelayMin;
+    [SerializeField] private float spawnDelayMax;
+    private float currentSpawnDelay;
+    private int spawnedEnemies;
 
     public override void StartRoomActivity()
     {
         LockRoom(true);
+        enemiesLeft = maxEnemies;
+        spawnedEnemies = 0;
+        for(int i=0;i<instantEnemies;i++)
+        {
+            SpawnBasicEnemy();
+        }
     }
     private void LockRoom(bool lock_)
     {
         lockObject.SetActive(lock_);
+    }
+    private void Update()
+    {
+        if(isLocked)
+        {
+            if (enemiesLeft <= 0)
+            {
+                LockRoom(false);
+                return;
+            }
+            currentSpawnDelay -= Time.deltaTime;
+            if (currentSpawnDelay >= 0)
+            {
+                currentSpawnDelay = Random.Range(spawnDelayMin, spawnDelayMax);
+            }
+
+           
+        }
+    }
+
+    private void SpawnBasicEnemy()
+    {
+        if (spawnedEnemies >= maxEnemies)
+            return;
+        Transform pos = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        GameObject obj = Instantiate(myDungeon.regularEnemies[Random.Range(0, myDungeon.regularEnemies.Count)].gameObject,pos.position,pos.rotation);
+        obj.SetActive(true);
+        spawnedEnemies += 1;
     }
 }
