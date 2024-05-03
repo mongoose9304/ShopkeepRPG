9d832043f536f98b164ab1fa1c5278090d2272b1
mongoose9304 <rymaster_109@hotmail.com>
2024-05-03 15:20:47 -0400

    little bit of code i forgot to add to push

--- Assets/Scripts/UI/Inventory/TabButton.cs
+++ Assets/Scripts/UI/Inventory/TabButton.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[[RequireComponent(typeof(Image))]]
 public class TabButton : MonoBehaviour
 {
+    public TabGroup tabGroup;
+    public Image background;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        background = GetComponent<Image>();
+        tabGroup.SubToList(this);
     }
 
     // Update is called once per frame
