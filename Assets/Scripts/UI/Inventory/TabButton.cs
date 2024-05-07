0c03482a256635daedaf4ee0b63d888f685f3fa7
mongoose9304 <rymaster_109@hotmail.com>
2024-05-07 10:49:53 -0400

    fixed a UI bug i made

--- Assets/Scripts/UI/Inventory/TabButton.cs
+++ Assets/Scripts/UI/Inventory/TabButton.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-[[RequireComponent(typeof(Image))]]
+[RequireComponent(typeof(Image))]
 public class TabButton : MonoBehaviour
 {
     public TabGroup tabGroup;
