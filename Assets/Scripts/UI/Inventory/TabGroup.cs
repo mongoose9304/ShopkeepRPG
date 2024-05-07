9a8ae3687b69fdb83bdba6c25a731f54f17b294e
mongoose9304 <rymaster_109@hotmail.com>
2024-05-07 11:53:04 -0400

    UI controls almost done

--- Assets/Scripts/UI/Inventory/TabGroup.cs
+++ Assets/Scripts/UI/Inventory/TabGroup.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class PageHolder{
+    public GameObject page;
+}
+
 public class TabGroup : MonoBehaviour
 {
     public List<TabButton> tabButtons;
+    public List<GameObject> pages;
     public Sprite tabIdle;
     public Sprite tabHover;
     public Sprite tabSelected;
+    public TabButton selectedTab;
 
     public void ResetTabs(){
         foreach (TabButton button in tabButtons){
+            if (selectedTab != null && button == selectedTab){
+                continue;
+            }
             button.background.sprite = tabIdle;
         }
     }
@@ -27,7 +37,9 @@ public class TabGroup : MonoBehaviour
 
     public void OnTabEnter(TabButton tabButton){
         ResetTabs();
-        tabButton.background.sprite = tabHover;
+        if (selectedTab == null || tabButton != selectedTab){
+            tabButton.background.sprite = tabHover;
+        }
     }
     
     public void OnTabExit(TabButton tabButton){
@@ -35,8 +47,18 @@ public class TabGroup : MonoBehaviour
     }
 
     public void OnTabSelected(TabButton tabButton){
+        selectedTab = tabButton;
         ResetTabs();
         tabButton.background.sprite = tabSelected;
+        int index = tabButton.transform.GetSiblingIndex();
+        for(int i = 0; i < pages.Count; i++) {
+            if (i == index){
+                pages[i].SetActive(true);
+            }
+            else {
+                pages[i].SetActive(false);
+            }
+        }
     }
 
 
