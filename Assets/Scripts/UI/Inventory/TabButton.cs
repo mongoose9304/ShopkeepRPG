06b830863dfd4968def80d675c24f8e2224bbf69
mongoose9304 <rymaster_109@hotmail.com>
2024-05-07 11:12:52 -0400

    More UI
    Aiming to finish inventory UI this week

--- Assets/Scripts/UI/Inventory/TabButton.cs
+++ Assets/Scripts/UI/Inventory/TabButton.cs
@@ -5,10 +5,25 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Image))]
-public class TabButton : MonoBehaviour
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public TabGroup tabGroup;
     public Image background;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
 
     // Start is called before the first frame update
     void Start()
