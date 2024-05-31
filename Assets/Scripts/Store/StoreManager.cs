7078f3d92f011da17b09c1a4b2b3fd9568d95630
mongoose9304 <rymaster_109@hotmail.com>
2024-05-31 14:47:15 -0400

    store planning

--- Assets/Scripts/Store/StoreManager.cs
+++ Assets/Scripts/Store/StoreManager.cs
@@ -16,4 +16,35 @@ public class StoreManager : MonoBehaviour
     // We need to know the specific checkout pedestal
     public GameObject checkoutPedestal;
 
+    public GameObject storeUI;
+
+    public void Start(){
+
+    }
+    
+    public void Update(){
+
+    }
+
+    public void StartStore(){
+        // Show the store UI
+        storeUI.SetActive(true);
+
+        // pick a random item from the item pedestals array that is not empty
+        
+        // update the store ui to show the item and the base price
+
+        // wait for the player to press the sell button
+
+        // decide if the item is in the range that the npc will pay for (basePrice * NpcCofficient) > price > (basePrice * -NpcCoefficient) 
+
+        // if it is then add the price to the player's inventory and remove the item from the pedestal
+
+        // if not then npc gets +1 to anger and the player can try again, if the player runs out of tries then the npc leaves and the player keeps the item
+
+        // repeat a few times then return control to the player
+    }
+    
+
+
 }
