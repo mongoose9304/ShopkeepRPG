d1d18f48742645f70a94a8fca531399afa6323dc
RobertWard <robert.blank.wardo@gmail.com>
2024-06-17 15:41:47 -0400

    bombs can trigger each other

--- Assets/Scripts/Mining/Explosion.cs
+++ Assets/Scripts/Mining/Explosion.cs
@@ -5,4 +5,26 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     [SerializeField] float lifetime;
+    float currentLifetime;
+
+    private void Update()
+    {
+        currentLifetime -= Time.deltaTime;
+        if(currentLifetime<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        currentLifetime = lifetime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Bomb")
+        {
+            other.GetComponent<Bomb>().Explode();
+        }
+    }
 }
