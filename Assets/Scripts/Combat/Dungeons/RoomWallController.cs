61e7f1aad270a18890b910c0b65179cffd6c5ed0
RobertWard <robert.blank.wardo@gmail.com>
2024-08-13 14:32:27 -0400

    automatic doors added

--- Assets/Scripts/Combat/Dungeons/RoomWallController.cs
+++ Assets/Scripts/Combat/Dungeons/RoomWallController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class RoomWallController : MonoBehaviour
 {
     [SerializeField] List<GameObject> doors = new List<GameObject>();
+    [SerializeField] List<GameObject> connectors = new List<GameObject>();
     [SerializeField] List<GameObject> wallsNoDoors = new List<GameObject>();
     [SerializeField] List<GameObject> wallsWDoors = new List<GameObject>();
     [SerializeField] bool manualSetUp;
     [SerializeField] List<bool> manualActiveWallsWithDoors = new List<bool>();
     [SerializeField] List<bool> manualActiveWallsWithoutDoors = new List<bool>();
+    [SerializeField] LayerMask connectorLayer;
     private void Start()
     {
         SetUpRoom();
@@ -23,6 +25,44 @@ public class RoomWallController : MonoBehaviour
             ToggleWallsWithDoors(manualActiveWallsWithDoors);
             ToggleWallsWithoutDoors(manualActiveWallsWithoutDoors);
         }
+        else
+        {
+            CheckConnections();
+        }
+
+    }
+    private void CheckConnections()
+    {
+        if (manualSetUp)
+            return;
+
+             List<bool> x= new List<bool>();
+        foreach(GameObject obj in connectors)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(obj.transform.position, 2,connectorLayer);
+            if(hitColliders.Length==1)
+            {
+                x.Add(false);
+                continue;
+            }
+            foreach (var hitCollider in hitColliders)
+            {
+                if (connectors.Contains(hitCollider.gameObject))
+                {
+                }
+                else
+                    x.Add(true);
+            }
+           
+           
+        }
+        Debug.Log(x.Count);
+            ToggleWallsWithDoors(x);
+       for(int i=0;i<x.Count; i++)
+        {
+            x[i] = !x[i];
+        }
+        ToggleWallsWithoutDoors(x);
 
     }
     private void ResetRoom()
