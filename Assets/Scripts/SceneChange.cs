9c9cb7e1670b9fe053c4b830756ac0b381f6f238
mongoose9304 <rymaster_109@hotmail.com>
2024-08-20 13:29:18 -0400

    scene transition

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
