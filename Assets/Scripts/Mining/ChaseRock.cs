2609994dd217e941f9cfe23c55cef4836a92cd4d
RobertWard <robert.blank.wardo@gmail.com>
2024-10-22 15:26:39 -0400

    little polish

--- Assets/Scripts/Mining/ChaseRock.cs
+++ Assets/Scripts/Mining/ChaseRock.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class ChaseRock : MonoBehaviour
 {
     public float damage;
+    public GameObject rockExplosionParticleEffect;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Player")
         {
             other.GetComponent<MiningPlayer>().TakeDamage(damage);
         }
-        if (other.tag == "Wall")
+        if (other.tag =="Wall")
         {
             other.gameObject.SetActive(false);
+            GameObject.Instantiate(rockExplosionParticleEffect, other.transform.position, Quaternion.Euler(-90, 0, 0));
+            Debug.Log("Rock hits wall");
         }
         if (other.tag == "Rock")
         {
             other.gameObject.SetActive(false);
+            GameObject.Instantiate(rockExplosionParticleEffect, other.transform.position, Quaternion.Euler(-90, 0, 0));
         }
         if (other.tag == "EndZone")
         {
