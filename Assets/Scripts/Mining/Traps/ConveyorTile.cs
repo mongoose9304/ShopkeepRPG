65bd6ccdb277892da98d2b7b1c1e6413ceb81f4a
RobertWard <robert.blank.wardo@gmail.com>
2024-10-23 16:55:01 -0400

    layouts and build

--- Assets/Scripts/Mining/Traps/ConveyorTile.cs
+++ Assets/Scripts/Mining/Traps/ConveyorTile.cs
@@ -7,11 +7,27 @@ public class ConveyorTile : MonoBehaviour
     public float moveSpeed;
     public float dampModifier;
     Vector3 velocity = Vector3.zero;
+    public List<GameObject> objectsInZone = new List<GameObject>();
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Explosion")
             return;
-        other.transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        if(!objectsInZone.Contains(other.gameObject))
+        objectsInZone.Add(other.gameObject);
+        
         //other.transform.position = Vector3.SmoothDamp(other.transform.position, other.transform.position + (transform.forward*moveSpeed * Time.fixedDeltaTime), ref velocity, dampModifier);
     }
+ 
+    private void Update()
+    {
+        foreach(GameObject obj in objectsInZone)
+        {
+            obj.transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        objectsInZone.Clear();
+    }
+
 }
