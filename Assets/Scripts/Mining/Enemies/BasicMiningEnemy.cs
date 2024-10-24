1e5a841dc469bd206196d0c0cb90eca1b4e1b4a4
RobertWard <robert.blank.wardo@gmail.com>
2024-10-24 11:49:33 -0400

    more audios

--- Assets/Scripts/Mining/Enemies/BasicMiningEnemy.cs
+++ Assets/Scripts/Mining/Enemies/BasicMiningEnemy.cs
@@ -1,3 +1,4 @@
+using MoreMountains.Tools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +15,8 @@ public class BasicMiningEnemy : MonoBehaviour
     [Header("References")]
     public GameObject player;
     [SerializeField] protected float currentHealth;
-
+    [SerializeField] protected AudioClip deathAudio;
+    [SerializeField] protected AudioClip attackAudio;
     protected virtual void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -45,5 +47,9 @@ public class BasicMiningEnemy : MonoBehaviour
     {
         lootDropper.DropItems();
         Destroy(gameObject);
+        if(deathAudio)
+        MMSoundManager.Instance.PlaySound(deathAudio, MMSoundManager.MMSoundManagerTracks.Sfx, transform.position,
+    false, 1.0f, 0, false, 0, 1, null, false, null, null, Random.Range(0.95f, 1.05f), 0, 0.0f, false, false, false, false, false, false, 128, 1f,
+    1f, 0, AudioRolloffMode.Logarithmic, 1f, 500f, false, 0f, 0f, null, false, null, false, null, false, null, false, null);
     }
 }
--- Assets/Scripts/Mining/Enemies/HideEnemyHat.cs
+++ Assets/Scripts/Mining/Enemies/HideEnemyHat.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Feedbacks;
+using MoreMountains.Tools;
+
 public class HideEnemyHat : MonoBehaviour
 {
     [SerializeField] MMF_Player AttackEffect;
     [SerializeField] float damage;
+    [SerializeField] AudioClip attackAudio;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Player")
         {
             other.GetComponent<MiningPlayer>().TakeDamage(damage);
             AttackEffect.PlayFeedbacks();
+            if (attackAudio)
+                MMSoundManager.Instance.PlaySound(attackAudio, MMSoundManager.MMSoundManagerTracks.Sfx, transform.position,
+            false, 1.0f, 0, false, 0, 1, null, false, null, null, Random.Range(0.95f, 1.05f), 0, 0.0f, false, false, false, false, false, false, 128, 1f,
+            1f, 0, AudioRolloffMode.Logarithmic, 1f, 500f, false, 0f, 0f, null, false, null, false, null, false, null, false, null);
         }
     }
 }
