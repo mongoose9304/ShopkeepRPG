f2b029da10f5b2001671bb08df15f630ed15b271
mongoose9304 <rymaster_109@hotmail.com>
2024-11-15 16:47:52 -0500

    test new rig, next up schedules

--- Assets/Scripts/Town/NPCS/NPCNavMesh.cs
+++ Assets/Scripts/Town/NPCS/NPCNavMesh.cs
@@ -7,8 +7,22 @@ public class NPCNavMesh : MonoBehaviour
 {
     private NavMeshAgent agent;
 
+    // testing for now, this will be every location in the game
+    // need to make a schedule for the npcs
+    [SerializeField]
+    private List<GameObject> waypoints = new List<GameObject>();
+
+    [SerializeField]
+    private Animator anim;
+
     [SerializeField]
     private Transform NPCtarget;
+
+    private void Start() {
+        if (anim == null) {
+            anim = GetComponent<Animator>();
+        }
+    }
 
     private void Awake() {
         agent = GetComponent<NavMeshAgent>();
@@ -17,6 +31,18 @@ public class NPCNavMesh : MonoBehaviour
     private void Update() {
         // just for testing
         agent.destination = NPCtarget.position;
+        bool isMoving = agent.remainingDistance > agent.stoppingDistance;
+
+        if (!isMoving){
+            //check if waypoints aren't empty, choose random waypoint
+            if (waypoints.Count > 0) {
+                int randomIndex = Random.Range(0, waypoints.Count);
+                NPCtarget.position = waypoints[randomIndex].transform.position;
+            }
+        }
+
+        anim.SetBool("isWalking", isMoving);
+
     }
 
 }
