7c703508efc0350e7cf04714bdbfd8be662978ec
RobertWard <robert.blank.wardo@gmail.com>
2024-12-05 12:48:04 -0500

    mining scene fixed

--- Assets/Scripts/Store/Rob/HellTeleporter.cs
+++ Assets/Scripts/Store/Rob/HellTeleporter.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class HellTeleporter : MonoBehaviour
 {
-    public bool inHell;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Player")
         {
-            if (inHell)
-                ShopManager.instance.ExitHell(other.gameObject);
-            else
-                ShopManager.instance.EnterHell(other.gameObject);
+             ShopManager.instance.WarpPlayerToOtherStore(other.gameObject);
+
         }
     }
 }
