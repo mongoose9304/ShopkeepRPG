d37f393a8692dbe2186edab561a18535c9e75239
RobertWard <robert.blank.wardo@gmail.com>
2024-12-19 12:27:50 -0500

    town music

--- Assets/Scripts/Town/TownManager.cs
+++ Assets/Scripts/Town/TownManager.cs
@@ -1,3 +1,4 @@
+using MoreMountains.Tools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class TownManager : MonoBehaviour
 {
-   public void QuitGame()
+    public List<AudioClip> BGMs = new List<AudioClip>();
+    private void Start()
+    {
+        PlayRandomBGM();
+    }
+
+    public void QuitGame()
     {
         Application.Quit();
     }
@@ -24,5 +31,10 @@ public class TownManager : MonoBehaviour
         string currentSceneName = SceneManager.GetActiveScene().name;
         TempPause.instance.TogglePause();
         SceneManager.LoadScene(currentSceneName);
+    }
+    public void PlayRandomBGM()
+    {
+        MMSoundManager.Instance.StopTrack(MMSoundManager.MMSoundManagerTracks.Music);
+        MMSoundManager.Instance.PlaySound(BGMs[Random.Range(0, BGMs.Count)], MMSoundManager.MMSoundManagerTracks.Music, Vector3.zero, true, 0.6f);
     }
 }
