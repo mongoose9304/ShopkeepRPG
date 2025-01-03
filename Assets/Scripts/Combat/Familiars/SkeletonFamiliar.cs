044a9e577d6033019bbca53ec7ce4df16ade99c0
RobertWard <robert.blank.wardo@gmail.com>
2025-01-03 10:58:50 -0500

    skeleton familiar works singleplayer

--- Assets/Scripts/Combat/Familiars/SkeletonFamiliar.cs
+++ Assets/Scripts/Combat/Familiars/SkeletonFamiliar.cs
@@ -8,8 +8,17 @@ public class SkeletonFamiliar : CombatFamiliar
     public MMMiniObjectPooler specialAttackPool;
     public float specialRange;
     public float AttackDistance;
+    public float RangedDistance;
     public float specialHexTime;
     public PlayerSkeletonMaster mySkeltonMaster;
+    public float ultDurationMax;
+    float ultDurationCurrent;
+    bool isUlting;
+    public GameObject ultObject;
+    public GameObject normalObject;
+    public MMMiniObjectPooler rangedAttackPool;
+    public Transform rangedSpawn;
+    public float ultimateAttackSpeed;
     protected override void Update()
     {
         if (TempPause.instance.isPaused)
@@ -34,15 +43,55 @@ public class SkeletonFamiliar : CombatFamiliar
             }
         }
     }
+    public override void UltimateAttack()
+    {
+        if (player.GetComponent<CombatPlayerActions>().isBusy || ultimateAttackCooldowncurrent > 0)
+            return;
+        ultDurationCurrent = ultDurationMax;
+        SetUltimateMode(true);
+        ultimateAttackCooldowncurrent = ultimateAttackCooldownMax;
+    }
+    public void SetUltimateMode(bool ult_)
+    {
+        if(ult_)
+        {
+            ultDurationCurrent = ultDurationMax;
+            isUlting = true;
+            normalObject.SetActive(false); 
+            ultObject.SetActive(true); 
+        }
+        else
+        {
+            ultDurationCurrent = 0;
+            isUlting = false;
+            normalObject.SetActive(true);
+            ultObject.SetActive(false);
+        }
+    }
     /// <summary>
     /// Cooldowns for attacks
     /// </summary>
     private void WaitForAttacks()
     {
 
-        AttackCooldowncurrent -= Time.deltaTime;
-        specialAttackCooldowncurrent -= Time.deltaTime;
-        ultimateAttackCooldowncurrent -= Time.deltaTime;
+      
+        if (isUlting)
+        {
+            AttackCooldowncurrent -= (Time.deltaTime*ultimateAttackSpeed);
+            specialAttackCooldowncurrent -= (Time.deltaTime*ultimateAttackSpeed);
+            if (ultDurationCurrent > 0)
+                ultDurationCurrent -= Time.deltaTime;
+            if(ultDurationCurrent<=0)
+            {
+                SetUltimateMode(false);
+            }
+        }
+        else
+        {
+            AttackCooldowncurrent -= Time.deltaTime;
+            specialAttackCooldowncurrent -= Time.deltaTime;
+            ultimateAttackCooldowncurrent -= Time.deltaTime;
+        }
         if (!target)
             return;
 
@@ -61,6 +110,21 @@ public class SkeletonFamiliar : CombatFamiliar
     }
     public override void Attack()
     {
+        if(isUlting)
+        {
+            if (Vector3.Distance(transform.position, target.transform.position) > RangedDistance)
+                return;
+            transform.LookAt(target.transform);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            GameObject obj = rangedAttackPool.GetPooledGameObject();
+            obj.transform.position = rangedSpawn.position;
+            obj.transform.rotation = rangedSpawn.rotation;
+            obj.GetComponent<MagicMissile>().damage = MysticalAtk;
+            obj.GetComponent<HomingAttack>().target = target.transform;
+            obj.SetActive(true);
+
+            return;
+        }
         if (Vector3.Distance(transform.position, target.transform.position) > AttackDistance)
             return;
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1);
