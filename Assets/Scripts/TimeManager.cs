e08f8598afef6e79748eeda501141ba937e51fcc
Gregori Diego <gregorid@mediastudies.ca>
2025-01-17 16:54:42 -0500

    TimeUpdate

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,61 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TimeManager;
 
 public class TimeManager : MonoBehaviour
 {
-    public enum TimeBlock{
-        Morning,
-        Noon,
-        Evening,
-        Night
-    }
+    public enum TimePeriod{ Morning, Noon, Evening, Night, EndPeriod };
+    int numTimePeriods = 5;
 
-    [SerializeField] Vector3[] sunPositions = new Vector3[4];
+    public enum Day{ Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    int numDays = 7;
 
-    [SerializeField] Transform dirLight;
-    [SerializeField] Light directionalLight;
-    [SerializeField] TimeBlock currentTimeBlock = TimeBlock.Morning;
+    public enum Week { First, Second, Third, Fourth };
+    int numWeeks = 4;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public enum Season { Spring, Summer, Autumn, Winter };
+    int numSeason = 4;
 
-    }
+    public int totalDays = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Shouldn't call this all the time, just here for debug purpose
-        UpdateEnvironment(currentTimeBlock);
-    }
+    public TimePeriod currentTimeBlock = TimePeriod.Morning;
+    public Day currentDay = Day.Sunday;
+    public Week currentWeek = Week.First;
+    public Season currentSeason = Season.Spring;
 
-    void UpdateEnvironment(TimeBlock timeBlock_){
-        switch (timeBlock_){
-            case TimeBlock.Morning:
-                SetSunLightColor(new Color(0.8f, 0.8f, 0.6f));
-                dirLight.rotation = Quaternion.Euler(sunPositions[0]);
-                break;
-            case TimeBlock.Noon:
-                SetSunLightColor(new Color(1f, 1f, 1f));
-                dirLight.rotation = Quaternion.Euler(sunPositions[1]);
-                break;
-            case TimeBlock.Evening:
-                SetSunLightColor(new Color(0.6f, 0.4f, 0.2f));
-                dirLight.rotation = Quaternion.Euler(sunPositions[2]);
-                break;
-            case TimeBlock.Night:
-                SetSunLightColor(new Color(0.1f, 0.1f, 0.2f));
-                dirLight.rotation = Quaternion.Euler(sunPositions[3]);
-                break;
-        }
-    }
 
-     void SetSunLightColor(Color colour)
-    {
-        if (directionalLight != null)
+    private void ProgressTime(int amount){
+        if(currentTimeBlock == TimePeriod.EndPeriod) 
         {
-            directionalLight.color = colour;
+            ProgressDay(1);
+            return;
         }
+        int newProgress = (int)currentTimeBlock + amount;
+        newProgress = Mathf.Clamp(newProgress, 0, numTimePeriods - 1);
+        currentTimeBlock = (TimePeriod)newProgress;      
+    }
+
+    private void ProgressDay(int days) 
+    {
+        totalDays += days;
+        int allocatedDays = 0;
+        while(allocatedDays < days) 
+        {
+            if (currentDay == Day.Sunday)
+            {
+                currentDay = Day.Monday;
+                ProgressWeek();
+                allocatedDays++;
+                continue;
+            }
+            int newProgress = (int)currentDay + 1;
+            currentDay = (Day)newProgress;
+            allocatedDays++;
+        }
+        currentTimeBlock = TimePeriod.Morning;
+    }
+
+    private void ProgressWeek()
+    {
+        if(currentWeek == Week.Fourth) 
+        {
+            currentWeek = Week.First;
+            ProgressSeason();
+        }
+
+        int newProgress = (int)currentWeek + 1;
+        int newProgressClamp = Mathf.Clamp(newProgress, 0, numWeeks - 1);
+
+        currentWeek = (Week)newProgress;
+        currentDay = Day.Monday;
+        currentTimeBlock = TimePeriod.Morning;
+    }
+
+    private void ProgressSeason()
+    {
+        if (currentSeason == Season.Winter)
+        {
+            currentSeason = Season.Spring;
+            return;
+        }
+
+        int newProgress = (int)currentSeason + 1;
+
+        currentSeason = (Season)newProgress;
+        currentWeek = Week.First;
+        currentDay = Day.Monday;
+        currentTimeBlock = TimePeriod.Morning;
+    }
+
+    public void PassTime() 
+    {
+        ProgressTime(1);
     }
 }
