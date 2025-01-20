59fc2cf9186c9efdb31249ab000a4e445fe62043
Delianomy <91752874+Delianomy@users.noreply.github.com>
2025-01-20 17:43:33 -0400

    Raycasting for the field of view
    Somewhat works. Sorry  i didnt do much progress here. It detects the player, but i think on the wrong side for now. I will work later on fixing that.
    
    I also added a new layout for the mining level. (used it as brain relaxation)

--- Assets/Scripts/Monsters/TrapChest_behaviour.cs
+++ Assets/Scripts/Monsters/TrapChest_behaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TrapChest_behaviour : MonoBehaviour
@@ -17,15 +18,23 @@ public class TrapChest_behaviour : MonoBehaviour
     private float rotationSpeed = 35.0f;
     private bool pauseRotation = false;
 
-    float rotationTick = 0.0f;
+    private float rotationTick = 0.0f;
 
-    float[] angles = { 0.0f, 45.0f, 90.0f, 135.0f, 180.0f };
-    int index = 0;
+    private float[] angles = { 0.0f, 45.0f, 90.0f, 135.0f, 180.0f };
+    private int index = 0;
+
+
+    //information needed for the field of view 
+    private float viewDistance = 40.0f;
+    private float viewAngle = 90.0f;
+    private int amountOfRays = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
         RayOrigin = transform.position;
-        RayDirection = new Vector3(10.0f, 0.0f, 0.0f);
+        RayDirection = transform.forward;
 
        targetAngle = angles[0];
     }
@@ -35,11 +44,45 @@ public class TrapChest_behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //i want to make the pause, rotate, pause, rotate movement for this mob.
+       
+
+        Patrol();
+        CastRays();
+ 
+
+    }
+
+    void CastRays()
+    {
+        //this is to ganerate the cone- like field of view. 
+        //Since for this mob we cant use simple colliders
+
+        float startingPoint = -viewAngle / 2;
+        float angleBetweenRays = viewAngle / amountOfRays;
+
+        for (int i = 0; i < amountOfRays; i++)
+        {
+            float newAngle = startingPoint + angleBetweenRays * i;
+            RayDirection = Quaternion.Euler(0, newAngle, 0) * transform.forward;
+
+            if (Physics.Raycast(RayOrigin, RayDirection, out RaycastHit hit, viewDistance))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("Player detected");
+                }
+
+            }
+
+
+        }
+    }
+    //i want to make the pause, rotate, pause, rotate movement for this mob.
+    void Patrol() {
 
         if (pauseRotation)
-        {   
-           
+        {
+
             timer += Time.deltaTime;
             if (timer >= rotationCD)
             {
@@ -52,23 +95,23 @@ public class TrapChest_behaviour : MonoBehaviour
                 targetAngle = angles[index];
             }
         }
-        else { 
+        else
+        {
             rotationTick = rotationSpeed * Time.deltaTime;
 
-            float angle  = Mathf.MoveTowards(currentAngle, targetAngle, rotationTick);
+            float angle = Mathf.MoveTowards(currentAngle, targetAngle, rotationTick);
 
             currentAngle = angle;
 
             transform.rotation = Quaternion.Euler(0.0f, currentAngle, 0.0f);
-      
+
 
             if (Mathf.Approximately(currentAngle, targetAngle))
             {
                 pauseRotation = true;
             }
         }
-
- 
-
     }
 }
+
+
