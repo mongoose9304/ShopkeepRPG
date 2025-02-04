94e1c5909a84e456f4e86d08bc86bfe7c40922ac
RobertWard <robert.blank.wardo@gmail.com>
2025-02-04 14:03:38 -0500

    slots chests added

--- Assets/Scripts/Combat/Loot/Chests/TreasureChest.cs
+++ Assets/Scripts/Combat/Loot/Chests/TreasureChest.cs
@@ -66,5 +66,4 @@ public class TreasureChest : InteractableObject
         playerInRange = inRange_;
 
     }
-    
 }
--- Assets/Scripts/Managers/DungeonManager.cs
+++ Assets/Scripts/Managers/DungeonManager.cs
@@ -148,7 +148,7 @@ public class DungeonManager : MonoBehaviour
         yield return new WaitForSeconds(0.001f);
         BasicDungeon d = GameObject.Instantiate(dungeonList[dungeonsCleared].gameObject, levelSpawn).GetComponent<BasicDungeon>();
         ChangeLevel(d);
-        yield return new WaitForSeconds(0.501f);
+        yield return new WaitForSeconds(1.501f);
         surface.BuildNavMesh();
         yield return new WaitForSeconds(0.01f);
         AddSinBlessing(currentSin);
