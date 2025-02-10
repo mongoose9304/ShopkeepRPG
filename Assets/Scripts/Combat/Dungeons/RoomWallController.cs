208b63471dd147c78911253b2d73bc4185df95c6
RobertWard <robert.blank.wardo@gmail.com>
2025-02-10 16:52:14 -0500

    adding walls rework

--- Assets/Scripts/Combat/Dungeons/RoomWallController.cs
+++ Assets/Scripts/Combat/Dungeons/RoomWallController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-///  Automatically opens and closes walls and doors based on if there is another room connected to this one. 
-///  There are small colliders called connectors, as long as 2 are touching they will automatically open both rooms
+///  prevents walls from overlaping onto each other by diabling one set 
 /// </summary>
 public class RoomWallController : MonoBehaviour
 {
