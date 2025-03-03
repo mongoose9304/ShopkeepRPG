9a9132f7e9fdbd3af5c0ea77fa9dccb50484be03
Dhafo <krigen.y@gmail.com>
2025-03-03 11:37:22 -0500

    placeable shop totem 1
    currently just placeable item, need to figure out the loading system to place on grid

---
+++ Assets/Sandbox/Yev/ShopTotem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTotem : MonoBehaviour
+{
+    public Vector2Int gridPos = Vector2Int.zero;
+
+    private void Start()
+    {
+        
+    }
+
+}
