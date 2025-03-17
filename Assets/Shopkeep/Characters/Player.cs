e302497bbfee9e2089031b6de8f0072046182960
Boykala <boykalagames@gmail.com>
2025-03-17 16:58:31 -0400

    Player1

--- Assets/Shopkeep/Characters/Player.cs
+++ Assets/Shopkeep/Characters/Player.cs
@@ -3,31 +3,62 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+class Spell {
+    public void Cast() {
+
+    }
+}
+
+
 public class Player : MonoBehaviour
 {
     CharacterController controller;
     InputActionMap inputMap;
 
+    Spell spellSlot1;
+    Spell spellSlot2;
+
     private void Start() {
         controller = GetComponent<CharacterController>();
         inputMap = GetComponent<PlayerInput>().currentActionMap;
+
+        inputMap.FindAction("XAction").performed += CastSpell1;
+        inputMap.FindAction("YAction").performed += CastSpell2;
+        inputMap.FindAction("AAction").performed += Attack;
     }
 
-    void MapInput() {
-        if(controller == null) return;
-        if(inputMap == null) return;
+    void MapInput(string action, System.Action<UnityEngine.InputSystem.InputAction.CallbackContext> onPerformed) {
+        if (controller == null) return;
+        if (inputMap == null) return;
 
         //map the input here
-        inputMap.FindAction("Action").performed += context => {
-            
-        };
-
-
+        inputMap.FindAction(action).performed += onPerformed;
     }
 
 
     public void EnableInput(InputAction input, bool b) {
         if (b) { input.Enable(); } 
         else { input.Disable(); }
+    }
+
+    private void Attack(InputAction.CallbackContext cont)
+    {
+
+    }
+
+    private void CastSpell1(InputAction.CallbackContext cont)
+    {
+        if (spellSlot1 != null)
+        {
+            spellSlot1.Cast();
+        }
+    }
+
+    private void CastSpell2(InputAction.CallbackContext cont)
+    {
+        if (spellSlot2 != null)
+        {
+            spellSlot2.Cast();
+        }
     }
 }
